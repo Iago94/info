71a93ba3db65410d4804a2d9fd37722388490542
Iago94 <93684809+Iago94@users.noreply.github.com>
2023-04-05 17:13:24 -0300

    Add files via upload

---
+++ iago/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+    return [
+        'develop' => [
+            'drive'     => 'postgres',
+            'host'      => 'localhost',
+            'database'  => 'iago_dev',
+            'user'      => 'postgres',
+            'pass'      => 'postgres',
+            'charset'   => 'utf-8',
+
+        ];
+    ]
